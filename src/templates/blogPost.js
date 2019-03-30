06c64daecfdf3a01186d93a09bbd7d2ffda1bf72
ShubhamModi004 <shubhammodi004@gmail.com>
2019-03-30 14:50:05 +0530

    new changes made and layout removed

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react'
 import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 
-import Layout from '../components/Layout';
 
 const blogPost = ({ data: { contentfulBlog}}) => {
     return (
-        <Layout>
             <div>
                 <h2>{contentfulBlog.header}</h2>
             </div>
-        </Layout>
     )
 }
 
