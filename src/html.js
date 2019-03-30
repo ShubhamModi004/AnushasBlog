ec1f6ff5829ac8f8db152d9b8ad8a307f1641271
ShubhamModi004 <shubhammodi004@gmail.com>
2019-03-30 16:01:06 +0530

    new changes made and layout added again and changes html file

--- src/html.js
+++ src/html.js
@@ -15,14 +15,13 @@ export default function HTML(props) {
         <link rel="shortcut icon" href="/icons/icon-48x48.png?v=07fab8ad5473f1e15e5f98b64f5d02eda19b8316" />
         <link rel="manifest" href="/manifest.webmanifest" />
         <meta name="theme-color" content="#000" />
-        <link rel="apple-touch-icon" sizes="48x48" href="./images/Logo.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="./images/Logo.png" />
-        <link rel="apple-touch-icon" sizes="96x96" href="./images/Logo.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="./images/Logo.png" />
-        <link rel="apple-touch-icon" sizes="192x192" href="./images/Logo.png" />
-        <link rel="apple-touch-icon" sizes="256x256" href="./images/Logo.png" />
-        <link rel="apple-touch-icon" sizes="384x384" href="./images/Logo.png" />
-        <link rel="apple-touch-icon" sizes="512x512" href="./images/Logo.png" />
+        <link rel="apple-touch-icon" sizes="48x48" href="./images/Icon.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="./images/Icon.png" />
+        <link rel="apple-touch-icon" sizes="96x96" href="./images/Icon.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="./images/Icon.png" />
+        <link rel="apple-touch-icon" sizes="192x192" href="./images/Icon.png" />
+        <link rel="apple-touch-icon" sizes="256x256" href="./images/Icon.png" />
+        <link rel="apple-touch-icon" sizes="384x384" href="./images/Icon.png" />
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
