bb352a9b7f4e916106bd48d3f56605e9846bb709
ShubhamModi004 <shubhammodi004@gmail.com>
2019-03-30 14:33:48 +0530

    Replace removed

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,37 +2,26 @@
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
-const PostTemplate = path.resolve('./src/templates/post-template.js');
 const BlogpostTemplate = path.resolve('./src/templates/blogPost.js');
 const PortfolioTemplate = path.resolve('./src/templates/portfolio-template.js');
 
-exports.onCreateNode = ({ node, getNode, actions }) => {
-    const { createNodeField } = actions
-    if (node.internal.type === 'MarkdownRemark') {
-        const slug = createFilePath({ node, getNode, basePath: 'posts'})
-        createNodeField({
-            node,
-            name: 'slug',
-            value: slug,
-        })
-    }
-}   
+// exports.onCreateNode = ({ node, getNode, actions }) => {
+//     const { createNodeField } = actions
+//     if (node.internal.type === 'MarkdownRemark') {
+//         const slug = createFilePath({ node, getNode, basePath: 'posts'})
+//         createNodeField({
+//             node,
+//             name: 'slug',
+//             value: slug,
+//         })
+//     }
+// }   
 
 
 exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
     const result = await graphql(`
         {
-            allMarkdownRemark{
-                edges{
-                    node{
-                    fields{
-                        slug
-                        }
-                    }
-                }
-                
-            }
             allContentfulBlog{
                 edges{
                     node {
@@ -76,17 +65,6 @@ exports.createPages = async ({ graphql, actions }) => {
         })
     })
 
-    
-    const posts = result.data.allMarkdownRemark.edges
-    posts.forEach(({ node: post }) => {
-        createPage({
-            path: `posts${post.fields.slug}`,
-            component: PostTemplate,
-            context: {
-                slug: post.fields.slug
-            }
-        })
-    })
 
 }
 
